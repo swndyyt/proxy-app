761a4c63c687145132baf28129b529e6e313daa0
swndyyt <swndyyt@inbox.ru>
2025-09-19 07:51:20 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, Response, request
+import requests
+
+app = Flask(__name__)
+
+TARGET_URL = "https://venya76.github.io/mines/"
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def proxy(path):
+    # если путь пустой → подставляем index.html
+    if path == "":
+        url = TARGET_URL + "index.html"
+    else:
+        url = TARGET_URL + path
+
+    resp = requests.get(url, params=request.args)
+
+    excluded_headers = ["content-encoding", "transfer-encoding", "connection"]
+    headers = [(name, value) for (name, value) in resp.headers.items() if name.lower() not in excluded_headers]
+
+    # убираем заголовки, блокирующие iframe
+    headers = [(name, value) for (name, value) in headers
+               if "frame" not in name.lower() and "content-security-policy" not in name.lower()]
+    headers.append(("X-Frame-Options", "ALLOWALL"))
+
+    return Response(resp.content, resp.status_code, headers)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
